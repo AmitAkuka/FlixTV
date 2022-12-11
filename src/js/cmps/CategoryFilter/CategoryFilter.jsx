2336d8b8c2d61cf8eb0b6f1175bb07b8f514db48
Amit Akoka <amitak@bynet.co.il>
2022-12-11 17:57:46 +0200

    Fixed category filter component arrows

--- src/js/cmps/CategoryFilter/CategoryFilter.jsx
+++ src/js/cmps/CategoryFilter/CategoryFilter.jsx
@@ -1,10 +1,13 @@
-import { useState, useRef } from 'react'
-import { useSelector } from 'react-redux'
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
+import { useState, useRef } from "react"
+import { useSelector } from "react-redux"
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos"
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew"
 
 export const CategoryFilter = ({ categories, onChangeGenereFilter }) => {
-  const [isRightEnd, setIsRightEnd] = useState(false)
+  const [scrollEnd, setScrollEnd] = useState({
+    isLeftEnd: true,
+    isRightEnd: false,
+  })
   const { filterBy } = useSelector((storeState) => storeState.showModule)
 
   const elementRef = useRef(null)
@@ -12,18 +15,22 @@ export const CategoryFilter = ({ categories, onChangeGenereFilter }) => {
   const onChangePage = (diff) => {
     if (!elementRef.current) return
     elementRef.current.scrollLeft += (elementRef.current.offsetWidth - 50) * diff
-    if (Math.abs(elementRef.current.scrollLeft) ===
-        (elementRef.current.scrollWidth - elementRef.current.clientWidth)){
-        setIsRightEnd(true)
-      }
-    if (elementRef.current.scrollLeft === 0) setIsRightEnd(false)
+    const calc = Math.abs(elementRef.current.scrollLeft) === elementRef.current.scrollWidth - elementRef.current.clientWidth
+    if (calc) {
+      setScrollEnd({ isLeftEnd: false, isRightEnd: true })
+    } else if (elementRef.current.scrollLeft === 0) {
+      setScrollEnd({ isLeftEnd: true, isRightEnd: false })
+    } else {
+      setScrollEnd({ isLeftEnd: false, isRightEnd: false })
+    }
   }
 
-  return <section className="main-category-filter-container main-layout">
+  return (
+    <section className="main-category-filter-container main-layout">
       <div ref={elementRef} className="categories-container">
         {categories.map((category) => (
           <button
-            className={category.genereId === filterBy.genereId ? 'active' : ''}
+            className={category.genereId === filterBy.genereId ? "active" : ""}
             key={category.genereId}
             onClick={() => onChangeGenereFilter(category)}
           >
@@ -31,11 +38,20 @@ export const CategoryFilter = ({ categories, onChangeGenereFilter }) => {
           </button>
         ))}
       </div>
-        <button className="prev-page-btn" style={{display: (isRightEnd) ? 'block' : 'none'}} onClick={() => onChangePage(-1)}>
-          <ArrowBackIosNewIcon />
-        </button>
-        <button className="next-page-btn" style={{display: (isRightEnd) ? 'none' : 'block'}} onClick={() => onChangePage(1)}>
-          <ArrowForwardIosIcon />
-        </button>
+      <button
+        className="prev-page-btn"
+        style={{ display: scrollEnd.isLeftEnd ? "none" : "block" }}
+        onClick={() => onChangePage(-1)}
+      >
+        <ArrowBackIosNewIcon />
+      </button>
+      <button
+        className="next-page-btn"
+        style={{ display: scrollEnd.isRightEnd ? "none" : "block" }}
+        onClick={() => onChangePage(1)}
+      >
+        <ArrowForwardIosIcon />
+      </button>
     </section>
+  )
 }
